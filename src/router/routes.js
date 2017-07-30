396407d21c0c4925587d4076ccb0b513936f9e67
Eryk Humberto Oliveira Alves <erykhumberto@hotmail.com>
2017-07-29 22:03:57 -0300

    Merge develop

---
+++ src/router/routes.js
@@ -0,0 +1,46 @@
+import Hello from '@/components/Hello'
+import About from '@/components/About'
+import YesNo from '@/components/YesNo'
+import Parent from '@/components/Parent'
+import BootstrapPlay from '@/components/BootstrapPlay'
+import Routing from '@/components/Routing'
+
+var routes = [
+  {
+    path: '/',
+    name: 'Hello',
+    component: Hello
+  },
+  {
+    path: '/yesno',
+    name: 'YesNo',
+    component: YesNo
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/bootstrap-play',
+    name: 'BootstrapPlay',
+    component: BootstrapPlay
+  },
+  {
+    path: '/emit-trigger',
+    name: 'Parent',
+    component: Parent
+  },
+  {
+    path: '/dynamic-routing',
+    name: 'Routing',
+    component: Routing
+  },
+  {
+    path: '/dynamic-routing/:id/routed/:routed_id',
+    name: 'RoutingPage',
+    component: Routing
+  }
+]
+
+export default routes
